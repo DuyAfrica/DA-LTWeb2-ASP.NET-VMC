ab2452a1f20cf34da0aa16cfb13628467d5a79aa
vmchau2903 <VuongMinhChau2903@gmail.com>
2017-06-15 12:16:26 +0700

    Danh mục

---
+++ DACK/DCP/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using DCP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DCP.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category/List
+        public ActionResult List()
+        {
+            using (var ctx = new QLDGEntities())
+            {
+                var list = ctx.Categories.ToList();
+
+                return PartialView("ListPartial", list);
+            }
+        }
+    }
+}
