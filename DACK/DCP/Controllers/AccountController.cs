fb72352646c0b5d8159a58ec5aa0360e904ad5ce
vmchau2903 <VuongMinhChau2903@gmail.com>
2017-06-17 14:46:24 +0700

    Đăng kí, MD5

--- DACK/DCP/Controllers/AccountController.cs
+++ DACK/DCP/Controllers/AccountController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using DCP.Helpers;
+using DCP.Models;
+using System;
 using System.Web.Mvc;
 
 namespace DCP.Controllers
@@ -19,6 +18,33 @@ namespace DCP.Controllers
         // GET: Account/Register
         public ActionResult Register()
         {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+
+        public ActionResult Register(RegisterVM model)
+        {
+
+            User u = new User
+            {
+                f_Username = model.Username,
+                f_Password = StringUtils.Md5(model.RawPW),
+                f_Name = model.Name,
+                f_DOB = DateTime.ParseExact(model.DOB, "d/m/yyyy", null),
+                f_Email = model.Email,
+                f_Address = model.Address,
+                f_Level = 0,
+                f_Rate = 100
+            };
+
+            using (var ctx = new QLDGEntities())
+            {
+                ctx.Users.Add(u);
+                ctx.SaveChanges();
+            }
+
             return View();
         }
     }
