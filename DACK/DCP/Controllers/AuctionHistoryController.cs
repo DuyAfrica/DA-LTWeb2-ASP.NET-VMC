c199f9613a317225250714e79a08aaca4af39c08
phi <huynhquachphi12@gmail.com>
2017-06-25 14:00:12 +0700

    Lịch sử đấu giá

---
+++ DACK/DCP/Controllers/AuctionHistoryController.cs
@@ -0,0 +1,27 @@
+﻿using DCP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DCP.Controllers
+{
+    public class AuctionHistoryController : Controller
+    {
+        // GET: AuctionHistory/List
+        public ActionResult List(int ?id)
+        {
+            using (var ctx = new QLDGEntities())
+            {
+
+                var list = ctx.AuctionHistories
+                    .Where(a => a.ProID == id)
+                    .ToList();
+
+                return PartialView("ListPartial", list);
+            }
+            
+        }
+    }
+}
